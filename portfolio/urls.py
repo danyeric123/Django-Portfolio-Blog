dd37d248170e5774fa7dbc4ec36dac8884c701fd
danyeric123 <dnagarpowers@gmail.com>
2021-07-27 19:28:28 -0400

    redirect done more efficiently

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # Adding a path to my projects.urls
-    path('', include('projects.urls')),
-    # path('projects/', include('projects.urls')),
+    path('', RedirectView.as_view(url="/projects")),
+    path('projects/', include('projects.urls')),
     path('blog/', include("blog.urls"))
 ]
