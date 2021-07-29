03d62ece669ad68656e5b17e8080ce2594e35892
danyeric123 <dnagarpowers@gmail.com>
2021-07-29 16:43:43 -0400

    bug fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,16 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     # My apps start here
+    'projects',
+    'blog',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    # My apps start here
-    'projects',
-    'blog',
 ]
 
 MIDDLEWARE = [
